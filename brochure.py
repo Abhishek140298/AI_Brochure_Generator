0cddf803ee7db537aea3f6c3906894216d6e7a5e
Abhishek140298 <abhishekyadavfeb1498@gmail.com>
2026-08-06 14:36:40 +0530

    Ai Brochure project >needs sub links also to create the good brochure for a web page ,so we will scrap out the sub links list ,and produce jsonfrom model

--- brochure.py
+++ brochure.py
@@ -1,8 +1,7 @@
 from openai import OpenAI
 from dotenv import load_dotenv
-from scraper import scrape_website
+from scraper import scrape_website,get_links
 import os
-
 load_dotenv()
 OPEN_API_KEY=os.getenv("OPENAI_API_KEY")
 client=OpenAI(api_key=OPEN_API_KEY)
@@ -31,5 +30,29 @@ def generate_brochure(url):
                                              )
     return response.choices[0].message.content
 
+def select_reslevant_links(url):
+    links=get_links(url)
+    links_system_promt=(
+                        "Yor are given a list of links found on the webpage. "
+                        "Decide which ones are relevant for building a company brochure. "
+                        "(e.g About, Comapny,Team Page) "
+                        "Ignore the things like Terms of Service ,Privacy Policy ,or social media links. "
+                        "Respond ONLY in JSON,in this exact format: \n"
+                        '{"links":[{"type":"about page,"url":"https://..."},]}'
+                        )
+    
+    links_user_prompt=(
+        f"Here are the list of the links from the {url}:\n{links}\n\nReturn the relvant ones as JSON"
+    )
+    response=client.chat.completions.messages(model="gpt-4o-mini",
+                                              messages=[{"role":"system","content":links_system_promt},
+                                                        {"role":"user","content":links_user_prompt}],
+                                              #resonse formate
+                                              response_format={"type":"json_object"}
+                                              )
+    result =json.loads(response.choice[0].message.content)  
+    return result["links"]
+ ##Json.loads
+    #!response_format in openai client
 brochure=generate_brochure("https://anthropic.com")
 print(brochure)
--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 def scrape_website(url):
     #*Fetch the website using the request
-    response=requests.get(url,{"User-Agent":"Nozilla/5.0"})
+    response=requests.get(url,{"User-Agent":"Mozilla/5.0"})
     
     soup=BeautifulSoup(response.content,"html.parser")
     
@@ -16,3 +16,12 @@ def scrape_website(url):
     
     return text,title
 
+
+
+def scrape_relevant_links(url):
+    #*Fetch the web from the server using request
+    response=requests.get(url,{"User-Agent":"Mozilla/5.0"})
+    
+    soup=BeautifulSoup(response.content,"html-parser")
+    links=[a.get("href") for a in soup.find_all("a") if a.get("href")]
+    return links
